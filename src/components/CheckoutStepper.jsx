29878afd618e70c6af13f0919e19a783450a650b
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-28 19:25:38 +0530

    adding a progress bar remaining and issue when setcompleted finish button appearing

--- src/components/CheckoutStepper.jsx
+++ src/components/CheckoutStepper.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react'
 
 function CheckoutStepper({ stepsConfig = [] }) {
-    const [step, setStep] = useState(4);
+    const [stepUser, setStepUser] = useState(1);
     const [completed, setCompleted] = useState(false)
+
+    let ActiveComponent = stepsConfig[stepUser-1].Component;
 
 
     function handleNext()
     {
-        console.log("mujhe kuch hua")
+        // console.log("mujhe kuch hua")
+        setStepUser((prev)=> {
+            if(prev=== stepsConfig.length)
+            {
+                setCompleted(prev => !prev)
+                return
+            }
+            else
+            {
+                return prev + 1
+            }
+        })
     }
 
     if (!stepsConfig.length)
@@ -16,19 +29,21 @@ function CheckoutStepper({ stepsConfig = [] }) {
         <>
             <div className='stepper'>
                 {
-                    stepsConfig.map((step, index) => <div key={step.name} className='step'>
+                    stepsConfig.map((step, index) => <div key={step.name} className={`step ${stepUser === index +1 ? "active" : ""} ${stepUser> index+1? "complete" : ""}`}>
 
-                        <div className='step-number'>{index + 1}</div>
+                        <div className='step-number'>{stepUser > index +1 ? <span>&#10003;</span> : index+1}</div>
                         <div className='step-name'>{step.name}</div>
 
                     </div>)
                 }
             </div>
+
+           <ActiveComponent/>
             {
                 !completed &&
 
                 // completed ?  <button>Finish</button> :  <button>Next</button>
-                <button className='btn' onClick={() => handleNext()}>{step === stepsConfig.length ? "Finish" : "Next"}</button>
+                <button className='btn' onClick={() => handleNext()}>{stepUser === stepsConfig.length ? "Finish" : "Next"}</button>
 
             }
         </>
